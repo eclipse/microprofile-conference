920b67b72b21fa9aeec70ace519cf9b18079ed13
AndyGee <andy.is@gmx.de>
2016-08-17 23:30:32 +0200

    Dummy files for GIT

---
+++ microservice-speaker/src/test/java/io/microprofile/showcase/speaker/DeleteMeTest.java
@@ -0,0 +1,17 @@
+package io.microprofile.showcase.speaker;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tomitribe Confidential
+ * <p/>
+ * Copyright(c) Tomitribe Corporation. 2014
+ * <p/>
+ * The source code for this program is not published or otherwise divested
+ * of its trade secrets, irrespective of what has been deposited with the
+ * U.S. Copyright Office.
+ * <p/>
+ */
+public class DeleteMeTest {
+
+}
