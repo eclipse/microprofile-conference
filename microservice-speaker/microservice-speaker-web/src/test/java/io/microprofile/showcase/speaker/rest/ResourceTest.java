03fbe809ec4200b690d35656ff063d40b745d7ea
AndyGee <andy.is@gmx.de>
2016-08-26 02:45:32 +0200

    License

--- microservice-speaker/microservice-speaker-web/src/test/java/io/microprofile/showcase/speaker/rest/ResourceTest.java
+++ microservice-speaker/microservice-speaker-web/src/test/java/io/microprofile/showcase/speaker/rest/ResourceTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.microprofile.showcase.speaker.rest;
 
 import io.microprofile.showcase.speaker.domain.ProducerVenue;
