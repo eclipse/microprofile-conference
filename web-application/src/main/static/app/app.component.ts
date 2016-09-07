49129fe817671ce8a6371a9fe4f7da52fb0dac0a
AndyGee <andy.is@gmx.de>
2016-09-07 22:44:41 +0200

    Angular2

---
+++ web-application/src/main/static/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'microprofile-conference',
+    template: '<h1>Microprofile Conference</h1>'
+})
+export class AppComponent { }
