af474920662fa535f07a28dd3603dc817084e237
AndyGee <andy.is@gmx.de>
2016-09-15 17:04:19 +0200

    Disable web.xml com.ibm.websphere.jaxrs.server.IBMRestServlet - Is this required for LP? Added JaxRs2Extension CXF workaround for Payara schedule service. Added service endpoints to UI app. Fix List injection in SpeakerDAO. Fixed some scopes.

---
+++ microservice-schedule/src/main/java/org/apache/cxf/jaxrs/swagger/JaxRs2Extension.java
@@ -0,0 +1,9 @@
+package org.apache.cxf.jaxrs.swagger;
+
+import io.swagger.jaxrs.ext.AbstractSwaggerExtension;
+/**
+ * Make application server classloaders happy.
+ * Will be fixed in CXF 3.2
+ */
+public class JaxRs2Extension extends AbstractSwaggerExtension {
+}
