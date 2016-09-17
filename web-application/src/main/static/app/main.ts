cadbf39612a996391a51e62b08aa5afaf8c7b250
AndyGee <andy.is@gmx.de>
2016-09-17 02:09:56 +0200

    Cleanup & start on vote/rating - Should refactor service a little more inline with other services

--- web-application/src/main/static/app/main.ts
+++ web-application/src/main/static/app/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-import "rxjs-operators";
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
