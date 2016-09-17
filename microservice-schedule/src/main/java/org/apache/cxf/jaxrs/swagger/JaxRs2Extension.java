e1d2cbbb1727781ad05d644206bfb2306ebc2ca1
Steve Millidge <smillidge@c2b2.co.uk>
2016-09-17 09:19:14 +0100

    Remove Swagger as it is bloating the Schedule Service 100x is not part of microprofile

--- microservice-schedule/src/main/java/org/apache/cxf/jaxrs/swagger/JaxRs2Extension.java
+++
@@ -1,9 +0,0 @@
-package org.apache.cxf.jaxrs.swagger;
-
-import io.swagger.jaxrs.ext.AbstractSwaggerExtension;
-/**
- * Make application server classloaders happy.
- * Will be fixed in CXF 3.2
- */
-public class JaxRs2Extension extends AbstractSwaggerExtension {
-}
