3a8e0e1cb9f8b89a78aa50a3b5acac5ec1a248c1
Werner Keil <werner.keil@gmx.net>
2017-02-06 22:58:18 +0100

    Update copyright in a few more files missed (POMs, .js and other parts of web-application)

--- web-application/src/main/static/gulpfile.js
+++ web-application/src/main/static/gulpfile.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Microprofile.io
+ * Copyright(c) 2016-2017 IBM, Red Hat, and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
