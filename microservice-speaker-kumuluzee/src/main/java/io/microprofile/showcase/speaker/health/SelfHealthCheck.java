04ea154ea2e3763da5b553137f8ab29805b6f58b
Urban Malc <urban.malc@cloud.si>
2017-09-29 21:57:54 +0200

    Added Microprofile Config and updated READMEs

--- microservice-speaker-kumuluzee/src/main/java/io/microprofile/showcase/speaker/health/SelfHealthCheck.java
+++ microservice-speaker-kumuluzee/src/main/java/io/microprofile/showcase/speaker/health/SelfHealthCheck.java
@@ -1,11 +1,13 @@
 package io.microprofile.showcase.speaker.health;
 
 import com.kumuluz.ee.common.config.EeConfig;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.logging.Logger;
@@ -13,6 +15,10 @@ import java.util.logging.Logger;
 @Health
 @ApplicationScoped
 public class SelfHealthCheck implements HealthCheck {
+
+    @Inject
+    @ConfigProperty(name = "health.health-check-http-method", defaultValue = "GET")
+    String healthCheckHttpMethod;
 
     private static final String port = EeConfig.getInstance().getServer().getHttp().getPort() + "";
     private static final String url = "http://localhost:" + port + "/speaker";
@@ -22,7 +28,7 @@ public class SelfHealthCheck implements HealthCheck {
     public HealthCheckResponse call() {
         try {
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("HEAD");
+            connection.setRequestMethod(healthCheckHttpMethod);
 
             if (connection.getResponseCode() == 200) {
                 return HealthCheckResponse.named(SelfHealthCheck.class.getSimpleName()).up().build();
